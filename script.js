0baa8d156ba147c00b11d5c3e6a2b987432839bf
Michael Perez <mapereztau@gmail.com>
2025-05-30 22:25:55 -0600

    actualizacion de datos

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarPagina(id) {
+  const paginas = document.querySelectorAll('.page');
+  paginas.forEach(pagina => pagina.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
+}
